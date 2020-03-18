6d50d2c15468191bb3b2457f7cf7f7ac7de789b9
Shawn Wang <shawnlcwang@gmail.com>
2020-03-17 23:18:38 -0700

    update

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const morgan = require('morgan');
 const debug = require('debug')('app');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const { Server } = require('ws');
+// const { Server } = require('ws');
 // const webpack = require('webpack');
 // const config = require('../webpack.cinfig.dev');
 
@@ -22,7 +22,7 @@ console.log(`./: ${process.cwd()}`); // path current working directory
 */
 const app = express();
 const port = process.env.PORT || 3000; // process.env REPLACE app.configure()
-const wss = new Server({ server });
+// const wss = new Server({ server });
 // const config = {};
 
 
